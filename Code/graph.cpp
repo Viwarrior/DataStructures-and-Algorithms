38222e3d8acf8b74a6abea2c6dd3bb8ec764f374
Avinash <avinashwc2011@gmail.com>
2020-12-04 19:07:55 +0530

    Update graph.cpp

--- Code/graph.cpp
+++ Code/graph.cpp
@@ -15,7 +15,7 @@ public:
     void bfs(int node){
         cout<<endl;
     int visited[10] = {0};
-    int distance[10] = {INT_MAX};
+    int distance[10] = {0};
     deque<int> q;
     q.push_front(node);
     distance[node] = 0;
@@ -61,6 +61,34 @@ public:
         depthsearch(node, visited);
     }
 
+    void dijkstra(int node){
+        cout<<endl;
+        int distance[10];
+        fill(distance, distance+10,500);
+        distance[node] =0;
+        int visited[10] = {0};
+        deque <pair<int, int>> p;
+        make_heap(p.begin(),p.end());
+        p.push_back({0,node});
+        push_heap(p.begin(),p.end());
+
+        while(!p.empty()){
+            pair<int,int> u = p.front();
+            p.pop_front();
+            pop_heap(p.begin(),p.end());
+            if(visited[u.second]) continue;
+            visited[u.second] = 1;
+            for(auto x: base[u.second]){
+                distance[x.first] = min(distance[x.first], distance[u.second]+x.second);
+                p.push_back({-1*distance[x.first],x.first});
+                push_heap(p.begin(),p.end());
+            }
+
+        }
+
+        for(int i=0; i<10; i++)
+            cout<<distance[i]<<" ";
+    }
 
 };
 
@@ -98,17 +126,68 @@ vector<tuple<int,int,int>> base;
 
 };
 
+
+class Graphmatrix{
+    int base[10][10];
+
+public:
+    Graphmatrix(){
+    for(int i=0;i<10; i++)
+    for(int j=0; j<10; j++)
+        base[i][j] = 500;
+    }
+
+    void show(){
+    for(int i=0;i<10; i++) {
+    for(int j=0; j<10; j++){
+        cout<<base[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    }
+
+    void add(int a, int b, int w){
+     base[a][b] = w;
+    }
+
+    void floydwarshall(){
+    int distance[10][10];
+    memcpy (distance, base, 10*10*sizeof(int));
+     for(int i=0;i<10;i++)
+        distance[i][i] = 0;
+
+
+     for(int k=0;k<10;k++)
+        for(int i=0;i<10; i++)
+     for(int j=0; j<10; j++){
+        distance[i][j] = min(distance[i][j], distance[i][k]+distance[k][j]);
+     }
+         for(int i=0;i<10; i++){
+     for(int j=0; j<10; j++){
+        cout<<distance[i][j]<<" ";
+     }
+     cout<<endl;
+    }
+    }
+
+
+};
+
+
 int main(){
 Graphlist g;
-g.add(0,1);
-g.add(0,7);
-g.add(0,5);
-g.add(1,2);
-g.add(1,4);
-g.add(4,3);
-g.add(2,3);
+g.add(0,1,2);
+g.add(0,7,5);
+g.add(0,5,6);
+g.add(1,2,1);
+g.add(1,4,3);
+g.add(4,3,7);
+g.add(2,3,2);
+
 g.dfs(0);
 g.bfs(0);
+g.dijkstra(0);
+
 
 Graphedgelist p;
 p.add(0,1,2);
@@ -119,6 +198,17 @@ p.add(1,4,3);
 p.add(4,3,7);
 p.add(2,3,2);
 
-p.bellmanford(1);
+p.bellmanford(0);
+cout<<endl;
+Graphmatrix  m;
+m.add(0,1,2);
+m.add(0,7,5);
+m.add(0,5,6);
+m.add(1,2,1);
+m.add(1,4,3);
+m.add(4,3,7);
+m.add(2,3,2);
+m.show();
+m.floydwarshall();
 return 0;
 }
